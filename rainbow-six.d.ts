3214c8dc25524430edf2eac62421b51e0012d91c
Omer Schapiro <omer.schapiro@overwolf.com>
2020-11-25 16:35:40 +0200

    hiding master breaking change

--- rainbow-six.d.ts
+++ rainbow-six.d.ts
@@ -1,51 +1,51 @@
 declare namespace overwolf.gep.R6 {
 
-    enum R6PTeam  {
+    enum R6PTeam {
         Orange = "Orange",
         Blue = "Blue"
     }
 
-    enum R6Outcome  {
+    enum R6Outcome {
         Victory = "victory",
         Defeat = "defeat"
     }
 
-    enum R6PGameMode  {
+    enum R6PGameMode {
         None = "NONE",
         MultiplayerNewcomer = "MATCHMAKING_PVP_NEWCOMER",
-        MultiplayerQuickMatch  = "MATCHMAKING_PVP",
+        MultiplayerQuickMatch = "MATCHMAKING_PVP",
         MultiplayerSpecialEvent = "MATCHMAKING_PVP_EVENT",
         MultiplayerArcadePlaylist = "MATCHMAKING_PVP_EVENT",
         MultiplayerRanked = "MATCHMAKING_PVP_UNRANKED",
         MultiplayerCustomGameLocal = "CUSTOMGAME_PVP",
-        MultiplayerCustomGameOnline  = "CUSTOMGAME_PVP_DEDICATED",
+        MultiplayerCustomGameOnline = "CUSTOMGAME_PVP_DEDICATED",
         PlayerVsAISituations = "OPERATIONS",
         PlayerVsAITrainingGroundsLonewolf = "MATCHMAKING_PVE_LONEWOLF",
-        PlayerVsAITrainingGroundsOnline = "MATCHMAKING_PVE_PARTY" 
+        PlayerVsAITrainingGroundsOnline = "MATCHMAKING_PVE_PARTY"
     }
-    
-    enum R6Phase  {
+
+    enum R6Phase {
         Lobby = "lobby",
         Teammates = "teammates",
         Announce = "announce",
         OperatorSelect = "operator_select",
         Loading = "loading",
-        RoundResults = "round_results"     
+        RoundResults = "round_results"
     }
 
 
     interface R6Player {
-        name?: string; 
-        team?: string; 
-        is_local?: boolean; 
-        operator?: number; 
-        kills?: number; 
-        deaths?: number; 
-        score?: number; 
-        health?: number; 
+        name?: string;
+        team?: string;
+        is_local?: boolean;
+        operator?: number;
+        kills?: number;
+        deaths?: number;
+        score?: number;
+        health?: number;
     }
 
-      // --------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
 
     /**
      * All available game info updates. When a new info update is triggered, it's one of these keys.
@@ -55,11 +55,11 @@ declare namespace overwolf.gep.R6 {
         pseudo_match_id?: string;
         game_mode?: R6PGameMode;
         number?: number;
-        score?: {blue?:number; orange?: number;}; //match feature
-        players?: { roster_0?: R6Player; roster_1?: R6Player; roster_2?: R6Player; roster_3?: R6Player;  };
+        // score?: {blue?:number; orange?: number;}; //match feature
+        players?: { roster_0?: R6Player; roster_1?: R6Player; roster_2?: R6Player; roster_3?: R6Player; };
         team?: R6PTeam;
         health?: number;
-        score?: number; //roster feature
+        // score?: number; //roster feature
         kills?: number;
         deaths?: number;
         operator?: string;
@@ -80,7 +80,7 @@ declare namespace overwolf.gep.R6 {
         knockedout?: null;
         death?: null;
         killer?: string;
- 
+
     }
 
     // --------------------------------------------------------------------------
@@ -96,5 +96,5 @@ declare namespace overwolf.gep.R6 {
      */
     type R6GameEvent2Event =
         overwolf.games.events.GameEvent2<R6GameEvents>;
-    
+
 }
