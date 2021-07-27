f2cce02e20d5b953bc992314eba43fc4c814c56b
eransharv <eransharv@gmail.com>
2021-07-27 14:38:15 +0300

    added the notification API

--- overwolf.d.ts
+++ overwolf.d.ts
@@ -283,7 +283,7 @@ declare namespace overwolf.cryptography {
   ): void;
 }
 
-declare namespace overwolf.media {
+declare namespace  {
   namespace enums {
     /**
      * Media type for the Media Event.
@@ -531,7 +531,7 @@ declare namespace overwolf.media {
 /**
  * @deprecated Since version 0.155.
  */
-declare namespace overwolf.media.audio {
+declare namespace .audio {
   type PlayState = "playing" | "stopped" | "paused";
 
   interface CreateResult extends Result {
@@ -636,7 +636,7 @@ declare namespace overwolf.media.audio {
   const onPlayStateChanged: Event<PlayStateChangedEvent>;
 }
 
-declare namespace overwolf.media.videos {
+declare namespace .videos {
   namespace enums {
     const enum WatermarkLocation {
       TopLeft = "topLeft",
@@ -769,7 +769,7 @@ declare namespace overwolf.media.videos {
 
 }
 
-declare namespace overwolf.media.replays {
+declare namespace .replays {
   namespace enums {
     const enum ReplayType {
       Video = "Video",
@@ -857,6 +857,7 @@ declare namespace overwolf.media.replays {
 
   interface ReplayServicesStartedEvent {
     extensions: string[];
+    is_game_window_capture?: boolean;
   }
 
   interface HighlightsCapturedEvent {
@@ -1083,6 +1084,94 @@ declare namespace overwolf.media.replays {
    * Fired when a new Replay highlight recorded (when highlightsSetting is enabled).
    */
   const onHighlightsCaptured: Event<HighlightsCapturedEvent>;
+}
+
+declare namespace overwolf.notifications  {
+  namespace enums {
+    const enum AppLogoCrop {
+      Default = "Default",
+      None = "None",
+      Circle = "Circle",
+    }
+
+    const enum ToatsEventType {
+      Dismiss = "dismiss",
+      ButtonClick = "buttonClick",
+      Error = "error",
+    }
+
+    const enum ToastEventError {
+      Unknown = "unknown",
+      NotificationsDisabled = "notificationsDisabled ",
+      Error = "error"
+    }
+  }
+
+  interface ToastNotificationParams { 
+    header: string;
+    /**
+     * Mandatory. Must include 1-3 texts (lines).
+     */
+    texts: string[];
+    /**
+     * By default, your toast will display your app's logo. However, you can override this logo with your own image.
+     */
+    logoOverride: LogoOverride;
+    /**
+     * Toasts can display a hero image, which is displayed prominently within the toast banner and while inside Action Center. Image dimensions must be 364x180 pixels.
+     */
+    heroImage: string;
+    /**
+     * You can provide a full-width inline-image that appears when you expand the toast.
+     */
+    inlineImage: string;
+    /**
+     * If you need to reference the source of your content, you can use attribution text. This text is always displayed at the bottom of your notification, along with your app's identity or the notification's timestamp.
+     */
+    attribution:string;
+    /**
+     * Buttons make your toast interactive, letting the user take quick actions on your toast notification without interrupting their current workflow. Buttons appear in the expanded portion of your notification.
+     */
+    buttons:ToastNotificationButton[];
+  }
+
+  interface LogoOverride {
+    url:string;
+    cropType: enums.AppLogoCrop;
+  }
+
+  interface ToastNotificationButton{
+    id: string;
+    text:string;
+  }
+
+  interface ShowToastNotificationResult extends Result {
+    id: string;
+  }
+
+  interface ToastNotificationEvent {
+    id: string;
+    eventType: enums.ToatsEventType;
+    buttonID:string;
+    error: string;
+    errorCode: enums.ToastEventError;
+  }
+
+  /**
+   * Fired when a user tapped on the body of a toast notification or performed an action inside a toast notification.
+   */
+   const onToastNotification: Event<ToastNotificationEvent>;
+   
+   /**
+   * Show Windows toast notification.
+   * @param args  Toast notification params
+   * @param callback A function called with the current user, or an error.
+   */
+  function showToastNotification(
+    args: ToastNotificationParams,
+    callback: CallbackFunction<ShowToastNotificationResult>
+  ): void;
+
 }
 
 declare namespace overwolf.profile {
@@ -3840,10 +3929,10 @@ declare namespace overwolf.streaming {
    * Defines the video source settings.
    */
   interface VideoSource {
-    source_type: overwolf.media.enums.eSourceType;
+    source_type: .enums.eSourceType;
     name: string;
     secondary_file: boolean; //source will be create on secondry video file(i.e another ow-obs.exe process will be createdw ith the same setting as the original one
-    transform: overwolf.media.enums.eVideoSourceTransform;
+    transform: .enums.eVideoSourceTransform;
   }
 
   /**
@@ -4057,6 +4146,7 @@ declare namespace overwolf.streaming {
   interface StreamEvent {
     stream_id?: number;
     SubErrorMessage?: string;
+    is_game_window_capture?: boolean;
   }
 
   interface GetWatermarkSettingsResult extends Result {
